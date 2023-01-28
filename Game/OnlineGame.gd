b59dbc639b976186038df761e247f57d8d8a8698
yello12 <brickbeacon123@gmail.com>
2023-01-27 22:21:16 -0800

    init the trilogy

--- Game/OnlineGame.gd
+++ Game/OnlineGame.gd
@@ -10,6 +10,7 @@ var players : Array = [
 var scores : Array = [0, 0]
 onready var t_label = $UI/Control/CenterContainer/VBoxContainer/TimeLabel
 onready var s_label = $UI/Control/CenterContainer/VBoxContainer/ScoreLabel
+const PLAYER_SCENE = preload("res://Game/Player.tscn")
 
 func _ready():
 	load_players()
@@ -22,8 +23,11 @@ func _ready():
 remotesync func load_players():
 	var self_id = get_tree().get_network_unique_id()
 	for player in Network.players:
-		pass
-	pass
+		var player_instance = PLAYER_SCENE.instance()
+		add_child(player_instance)
+		player_instance.name = Network.players.keys()[player]
+		if Network.players.keys()[player] == self_id:
+			player_instance.set_network_master(self_id)
 
 func _process(_delta):
 	pass
--- UI/OnlineLobby.gd
+++ UI/OnlineLobby.gd
@@ -1,24 +1,38 @@
 extends Control
 
 const PLAYER_CARD = preload("res://UI/PlayerLobbyCard.tscn")
+onready var team1 = $CenterContainer/VBoxContainer/HBoxContainer/Team1
+onready var team2 = $CenterContainer/VBoxContainer/HBoxContainer/Team2
+var is_team1 : bool = true
 
 func _ready():
 	#get_tree().connect("network_peer_connected", self,"_network_peer_connected")
 	get_tree().connect("network_peer_disconnected", self,"_network_peer_disconnected")
 	Network.connect("player_registered", self, "_Network_player_registered")
-	
-	for player_key in Network.players.keys():
-		print(Network.players)
-		add_player_card(player_key, Network.players[player_key].name, false)
+		
 	if Network.is_server:
 		$CenterContainer3/VBoxContainer/Server.disabled = false
-		
-func add_player_card(id, _name, is_ready):
+		for player_key in Network.players.keys():
+			Network.players[player_key]["is_team1"] = is_team1
+			is_team1 = !is_team1
+	rpc('sync_player_cards')
+
+remotesync func sync_player_cards():
+	for player_key in Network.players.keys():
+		add_player_card(player_key, Network.players[player_key].name, Network.players[player_key].is_team1, false)
+
+remotesync func sync_new_player_card(new_player_id):
+	add_player_card(new_player_id, Network.players[new_player_id].name, Network.players[new_player_id].is_team1, false)
+	
+func add_player_card(id, _name, is_team1 : bool, is_ready):
 	var card = PLAYER_CARD.instance()
 	card.id = id
 	card.set_name(_name)
 	card.set_ready(is_ready)
-	$CenterContainer/VBoxContainer.add_child(card)
+	if is_team1:
+		team1.add_child(card)
+	else:
+		team2.add_child(card)
 	card.name = str(id)
 
 func remove_player_card(id):
@@ -32,8 +46,10 @@ func _network_peer_disconnected(id):
 	remove_player_card(id)
 
 func _Network_player_registered(id):
-	add_player_card(id, Network.players[id].name, false)
-
+	if Network.is_server:
+		Network.players[id].is_team1 = is_team1
+		is_team1 = !is_team1
+		rpc('sync_new_player_card', id)
 
 func _on_Server_pressed():
 	Global.game_state = Global.ONLINEGAME
